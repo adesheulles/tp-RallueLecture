0202f9f3a43eeab65b4eec72905e2f1520ced02c
adesheulles <43208125+adesheulles@users.noreply.github.com>
2019-04-17 15:01:09 +0200

    Add files via upload

---
+++ code/Eleve.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppe
+{
+    class Eleve
+    {
+        private string nom;
+        private string prenom;
+        private string login;
+        private string passWord;
+       static Random random = new Random();
+        public Eleve(string nom, string prenom, string login,PassWordType type)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.login = login;
+            this.passWord = GetNewPassWord(type);
+        }
+        static private string GetPassWordAleatoire() // créer un mot de passe aléatoire
+        {
+            string minuscule = "abcdefghijklmnopqrstuvwxyz"; // liste des lettres minuscule
+            string majuscule = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // liste des lettres majuscule
+            string caractèreSpécial = "&@#"; // liste caractère spéciaux
+            string chiffre = "123456789"; // liste chiffre
+            string mdpAleatorie =""; 
+            
+            int positionMajuscule;
+            int positionChiffre;
+            int positionCaractèreSpécial;
+            positionMajuscule = random.Next(0, 8); // position de la lettre majuscule
+            positionChiffre = positionMajuscule; // on donne la position de la lettre majuscule à la position de chiffre
+                                                   // pour servire de condition et eviter que leur position soit egale
+            positionCaractèreSpécial = positionMajuscule;
+            while (positionChiffre == positionMajuscule) 
+            {
+                positionChiffre = random.Next(0, 8);
+            }
+            while (positionCaractèreSpécial == positionMajuscule)
+            {
+                positionCaractèreSpécial = random.Next(0, 8);
+            }
+            for (int i = 0; i < 9; i++) // boucle pour créer le mot de passe
+            {
+                if (i == positionMajuscule || i == positionChiffre || i==positionCaractèreSpécial)
+                {
+                    if (i == positionChiffre|| i==positionCaractèreSpécial)
+                    {
+                        if (i == positionChiffre) // choisit un chiffre aléatoire
+                        {
+                            mdpAleatorie += chiffre[random.Next(0, chiffre.Count())];
+                        }
+                        else // choisit un caractère spécial aléatoire
+                        {
+                            mdpAleatorie+=caractèreSpécial[random.Next(0,caractèreSpécial.Count())];
+                        }
+                    }
+                    else// choisit une majuscule aléatoire
+                    {
+                        mdpAleatorie += majuscule[random.Next(0,majuscule.Count())];
+                    }
+                }
+                else// choisit une minuscule aléatoire
+                {
+                    mdpAleatorie += minuscule[random.Next(0,minuscule.Count())];
+                }
+                
+            }
+            return mdpAleatorie;
+        } 
+        private string GetPassWordConstruit() // créer un mot de passe construit
+        {
+            string mdp;
+            mdp = Convert.ToString(this.prenom[0])+this.nom;
+            return mdp;
+        }
+        public string GetNewPassWord( PassWordType type)
+        {
+            string mdp;
+            switch (type)
+            {
+                case PassWordType.Aléatoire : // créer un mot de passe aléatoire
+                    mdp = GetPassWordAleatoire();
+                    return mdp;
+                case PassWordType.Construit: // créer un mot de passe construit
+                    mdp = this.GetPassWordConstruit();
+                    return mdp;
+                default: // créer un mot de passe aléatoire
+                    mdp = GetPassWordAleatoire();
+                    return mdp;
+
+            }
+        }
+        public new string ToString => string.Format("nom : {0} prenom : {1} login : {2} mdp : {3}", this.nom, this.prenom, this.login, this.passWord);
+        public string PassWord { get { return passWord; } }
+        public string Login { get { return login; } }
+        public string Prenom { get { return prenom; } }
+        public string Nom { get { return nom; } }
+
+    }
+}
---
+++ code/Hash.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ppe
+{
+    class Hash
+    {
+        public string GetHashPassword(string passWord)
+        {
+            //hash le mot de passe en SHA256
+			var message = Encoding.ASCII.GetBytes(passWord);
+            SHA256Managed hashString = new SHA256Managed();
+            string hex="";
+            var hashValue = hashString.ComputeHash(message);
+            foreach (byte x in hashValue)
+            {
+                hex += String.Format("{0:x2}", x);
+            }
+            return hex;
+             
+        }
+        public string Salt(string password, string salt)
+        {
+            // salage en md5 salt+password
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(salt);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                string passMD5 = sb.ToString();
+                passMD5 = passMD5.ToLowerInvariant();
+                string hashpassword = GetHashPassword(passMD5 + password);
+                return hashpassword;
+            }
+        }
+    }
+}
